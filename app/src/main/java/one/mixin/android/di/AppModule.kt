6ecdfad7fdfa303dcc4793303ace974bfb85e31a
SeniorZhai <developer.zhaitao@gmail.com>
2020-02-14 15:23:59 +0800

    Delay retry

--- app/src/main/java/one/mixin/android/di/AppModule.kt
+++ app/src/main/java/one/mixin/android/di/AppModule.kt
@@ -23,6 +23,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 import one.mixin.android.BuildConfig
 import one.mixin.android.Constants.API.GIPHY_URL
 import one.mixin.android.Constants.API.URL
+import one.mixin.android.Constants.DELAY_SECOND
 import one.mixin.android.MixinApplication
 import one.mixin.android.api.NetworkException
 import one.mixin.android.api.ServerErrorException
@@ -56,6 +57,7 @@ import one.mixin.android.job.MixinJobManager
 import one.mixin.android.job.MyJobService
 import one.mixin.android.util.LiveDataCallAdapterFactory
 import one.mixin.android.util.Session
+import one.mixin.android.util.Session.Companion.requestDelay
 import one.mixin.android.vo.CallState
 import one.mixin.android.vo.LinkState
 import one.mixin.android.websocket.ChatWebSocket
@@ -113,6 +115,14 @@ internal class AppModule {
                         if (this is SocketTimeoutException || this is UnknownHostException || this is ConnectException) {
                             HostSelectionInterceptor.get().switch()
                         }
+                    }
+                }
+
+                val authorization = response.request().header("Authorization")
+                if (!authorization.isNullOrBlank() && authorization.startsWith("Bearer ")) {
+                    val jwt = authorization.substring(7)
+                    if (requestDelay(Session.getAccount(), jwt, DELAY_SECOND)) {
+                        throw ServerErrorException(500)
                     }
                 }
 
